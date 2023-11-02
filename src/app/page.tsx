548ea500d307485cd5d794ea12093d0208535f61
paresiqbal <thecrowdel@gmail.com>
2023-11-02 20:19:02 +0700

    Add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
-import Image from "next/image";
+// Components
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Home() {
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegas: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
   return (
     <main>
-      <h1>Home</h1>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id}>
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time}</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter>
+              <button>View more</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
